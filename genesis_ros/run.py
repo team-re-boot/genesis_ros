95482b24d9faa1d8ae12c3358ef600dc4a4f043b
Masaya Kataoka <ms.kataoka@gmail.com>
2024-12-27 03:42:20 +0900

    enable get position
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- genesis_ros/run.py
+++ genesis_ros/run.py
@@ -4,11 +4,18 @@ gs.init(backend=gs.cpu)
 
 scene = gs.Scene(show_viewer=True)
 plane = scene.add_entity(gs.morphs.Plane())
-franka = scene.add_entity(
+robot = scene.add_entity(
     gs.morphs.URDF(file="/tmp/genesis_ros/model.urdf", fixed=True, pos=(0, 0, 0.4)),
 )
 
 scene.build()
 
+print(robot.links)
+body_link = robot.get_link("body_link")
+head_pan_link = robot.get_link("head_pan_link")
+
 for i in range(1000):
+    # print(robot.get_links_pos())
+    print(body_link.get_pos())
+    print(head_pan_link.get_pos())
     scene.step()
