5cfc57a2c41f7d230ceba213afc66c1f3ae56f55
Masaya Kataoka <ms.kataoka@gmail.com>
2024-12-27 15:35:54 +0900

    enable save rosbag
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- genesis_ros/run.py
+++ genesis_ros/run.py
@@ -1,4 +1,38 @@
 import genesis as gs
+from amber.importer.tf import TfImporter, TfImporterConfig
+from tf2_amber import (
+    TransformStamped,
+    Header,
+    Transform,
+    Time,
+    timeFromSec,
+    Vector3,
+    Quaternion,
+)
+import math
+
+
+def get_tf_from_link(cur_t, link):
+    TransformStamped(
+        Header(
+            Time(
+                int(math.floor(cur_t)),
+                int((cur_t - float(math.floor(cur_t))) * pow(10, 9)),
+            ),
+            "map",
+        ),
+        link.name,
+        Transform(
+            Vector3(link.get_pos()[0], link.get_pos()[1], link.get_pos()[2]),
+            Quaternion(
+                link.get_quat()[0],
+                link.get_quat()[1],
+                link.get_quat()[2],
+                link.get_quat()[3],
+            ),
+        ),
+    )
+
 
 gs.init(backend=gs.cpu)
 
@@ -11,11 +45,12 @@ robot = scene.add_entity(
 scene.build()
 
 print(robot.links)
-body_link = robot.get_link("body_link")
-head_pan_link = robot.get_link("head_pan_link")
 
-for i in range(1000):
-    # print(robot.get_links_pos())
-    print(body_link.get_pos())
-    print(head_pan_link.get_pos())
+importer = TfImporter(TfImporterConfig())
+
+for i in range(100):
+    importer.write(get_tf_from_link(scene.cur_t, robot.get_link("body_link")))
+    importer.write(get_tf_from_link(scene.cur_t, robot.get_link("head_pan_link")))
     scene.step()
+
+importer.finish()
