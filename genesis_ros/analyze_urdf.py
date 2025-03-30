b1468ac4e9abfbdf1158229df101faa7ec3884a2
Masaya Kataoka <ms.kataoka@gmail.com>
2025-03-30 23:47:45 +0900

    add CameraSensor class
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- genesis_ros/analyze_urdf.py
+++ genesis_ros/analyze_urdf.py
@@ -2,5 +2,18 @@ import xml.etree.ElementTree as ET
 from pathlib import Path
 
 
+class CameraSensor:
+    def __init__(self, link: str):
+        self.link = link
+
+
 def get_camera_sensors(urdf_path: Path):
     tree = ET.parse(urdf_path)
+    gazebo_elements = tree.getroot().findall(".//gazebo")
+    if gazebo_elements:
+        for gazebo_element in gazebo_elements:
+            sensor_elements = gazebo_element.findall(".//sensor")
+            if sensor_elements:
+                for sensor_element in sensor_elements:
+                    if sensor_element.attrib["type"] == "camera":
+                        CameraSensor(gazebo_element.attrib["reference"])
