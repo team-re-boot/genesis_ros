65b9d9cb40562a94f272c91ce240165449ed8999
Masaya Kataoka <ms.kataoka@gmail.com>
2025-03-31 00:04:05 +0900

    enable get look at point in camera sensor
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- genesis_ros/analyze_urdf.py
+++ genesis_ros/analyze_urdf.py
@@ -2,15 +2,17 @@ import xml.etree.ElementTree as ET
 from pathlib import Path
 from amber_mcap.tf2_amber import TransformStamped
 from genesis_ros import math
+import genesis as gs
 
 
 class CameraSensor:
-    def __init__(self, link: str):
-        self.link = link
+    def __init__(self, link_name: str):
+        self.link_name = link_name
 
-    def get_look_at_point(gs_link):
+    def get_look_at_point(gs_robot):
+        gs_link = robot.get_link(self.link_name)
         return math.get_look_at_point(
-            np.array(link.get_pos()), np.array(link.get_quat())
+            np.array(gs_link.get_pos()), np.array(gs_link.get_quat())
         )
 
 
