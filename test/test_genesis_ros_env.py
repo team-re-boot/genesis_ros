eb5127fb740c64442def1280539abbe3955b7637
Masaya Kataoka <ms.kataoka@gmail.com>
2025-04-13 16:44:27 +0900

    add pytest
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- test/test_genesis_ros_env.py
+++ test/test_genesis_ros_env.py
@@ -1,6 +1,27 @@
 import genesis as gs  # type: ignore
-from genesis_ros.genesis_ros_env import GenesisRosEnv  # type: ignore
+from genesis_ros.genesis_ros_env import GenesisRosEnv, genesis_entity
+from genesis_ros.genesis_ros_env_options import (
+    SimulationConfig,
+    EnvironmentConfig,
+    ObservationConfig,
+    RewardConfig,
+    CommandConfig,
+)
 
 
 def test_genesis_ros_env():
-    pass
+    gs.init(logging_level="warning", backend=gs.cpu)
+
+    @genesis_entity
+    def add_plane():
+        return gs.morphs.Plane()
+
+    env = GenesisRosEnv(
+        1,
+        SimulationConfig(),
+        EnvironmentConfig(),
+        ObservationConfig(),
+        RewardConfig(),
+        CommandConfig(),
+        "urdf/go2/urdf/go2.urdf",
+    )
