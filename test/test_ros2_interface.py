69ab74f02abe175db0b471f8a604ea0e7527811c
Masaya Kataoka <ms.kataoka@gmail.com>
2025-05-06 13:05:17 +0900

    add testcase for message conversion
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- test/test_ros2_interface.py
+++ test/test_ros2_interface.py
@@ -26,28 +26,64 @@ def test_torch_msgs_fp32_tensor():
 def test_torch_msgs_fp64_tensor():
     msg = torch_msgs.msg.FP64Tensor(is_cuda=False, data=[0.0], shape=[1])
     msg.serialize()
+    assert isinstance(
+        torch_msgs.msg.from_torch_tensor(
+            torch.tensor([1.0, 2.0, 3.0, 4.5], dtype=torch.float64)
+        ),
+        torch_msgs.msg.FP64Tensor,
+    )
 
 
 def test_torch_msgs_int16_tensor():
     msg = torch_msgs.msg.INT16Tensor(is_cuda=False, data=[0], shape=[1])
     msg.serialize()
+    assert isinstance(
+        torch_msgs.msg.from_torch_tensor(
+            torch.tensor([1.0, 2.0, 3.0, 4.5], dtype=torch.int16)
+        ),
+        torch_msgs.msg.INT16Tensor,
+    )
 
 
 def test_torch_msgs_int32_tensor():
     msg = torch_msgs.msg.INT32Tensor(is_cuda=False, data=[0], shape=[1])
     msg.serialize()
+    assert isinstance(
+        torch_msgs.msg.from_torch_tensor(
+            torch.tensor([1.0, 2.0, 3.0, 4.5], dtype=torch.int32)
+        ),
+        torch_msgs.msg.INT32Tensor,
+    )
 
 
 def test_torch_msgs_int64_tensor():
     msg = torch_msgs.msg.INT64Tensor(is_cuda=False, data=[0], shape=[1])
     msg.serialize()
+    assert isinstance(
+        torch_msgs.msg.from_torch_tensor(
+            torch.tensor([1.0, 2.0, 3.0, 4.5], dtype=torch.int64)
+        ),
+        torch_msgs.msg.INT64Tensor,
+    )
 
 
 def test_torch_msgs_int8_tensor():
     msg = torch_msgs.msg.INT8Tensor(is_cuda=False, data=[0], shape=[1])
     msg.serialize()
+    assert isinstance(
+        torch_msgs.msg.from_torch_tensor(
+            torch.tensor([1.0, 2.0, 3.0, 4.5], dtype=torch.int8)
+        ),
+        torch_msgs.msg.INT8Tensor,
+    )
 
 
 def test_torch_msgs_uint8_tensor():
     msg = torch_msgs.msg.UINT8Tensor(is_cuda=False, data=[0], shape=[1])
     msg.serialize()
+    assert isinstance(
+        torch_msgs.msg.from_torch_tensor(
+            torch.tensor([1.0, 2.0, 3.0, 4.5], dtype=torch.uint8)
+        ),
+        torch_msgs.msg.UINT8Tensor,
+    )
