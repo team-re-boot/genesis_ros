6110c83b7cf2fa02633edd85e10ee873224d09d5
Masaya Kataoka <ms.kataoka@gmail.com>
2025-06-23 21:38:17 +0900

    add reward function
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- genesis_ros/ppo/config/h1_walking/reward_functions.py
+++ genesis_ros/ppo/config/h1_walking/reward_functions.py
@@ -8,31 +8,9 @@ def get_reward_functions():
     # ------------ reward functions----------------
     def reward_base_height(self):
         # Penalize base height away from target
-        return torch.square(self.base_pos[:, 2] - 0.8)
+        return torch.square(self.base_pos[:, 2] - 0.9)
 
     reward_functions.append((reward_base_height, -1.0))
-
-    # def penalty_base_height(self):
-    #     # Penalize base height away from target
-    #     return torch.square(self.base_pos[:, 2] - 0.8)
-
-    # reward_functions.append((penalty_base_height, -1.0))
-
-    # def reward_tracking_lin_vel(self):
-    #     # Tracking of linear velocity commands (xy axes)
-    #     lin_vel_error = torch.sum(
-    #         torch.square(self.commands[:, :2] - self.base_lin_vel[:, :2]), dim=1
-    #     )
-    #     return torch.exp(-lin_vel_error / 0.25 + penalty_base_height(self))
-
-    # reward_functions.append((reward_tracking_lin_vel, 10.0))
-
-    # def reward_tracking_ang_vel(self):
-    #     # Tracking of angular velocity commands (yaw)
-    #     ang_vel_error = torch.square(self.commands[:, 2] - self.base_ang_vel[:, 2])
-    #     return torch.exp(-ang_vel_error / 0.25 + penalty_base_height(self))
-
-    # reward_functions.append((reward_tracking_ang_vel, 0.2))
 
     def reward_lin_vel_z(self):
         # Penalize z axis base linear velocity
@@ -51,5 +29,13 @@ def get_reward_functions():
         return torch.sum(torch.abs(self.dof_pos - self.default_dof_pos), dim=1)
 
     reward_functions.append((reward_similar_to_default, -0.1))
+
+    def reward_base_roll_pitch(self):
+        # Penalize base roll and pitch angles
+        pitch = self.base_euler[:, 1]
+        roll = self.base_euler[:, 0]
+        return 1 / torch.square(pitch) + torch.square(roll)
+
+    reward_functions.append((reward_base_roll_pitch, 0.1))
 
     return reward_functions
